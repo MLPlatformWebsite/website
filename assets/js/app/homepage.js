7a7bbfe1c3e4f19309727c79798d2842a9616850
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-28 14:36:44 +0000

    Removed comments

--- assets/js/app/homepage.js
+++ assets/js/app/homepage.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
-
     // Gets the video src from the data-src on each button
     var $videoSrc;
     $('.video-btn').click(function () {
         $videoSrc = $(this).data("src");
     });
     console.log($videoSrc);
-
     // when the modal is opened autoplay it  
     $('#myModal').on('shown.bs.modal', function (e) {
         // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
@@ -17,5 +15,4 @@ $(document).ready(function () {
         // a poor man's stop video
         $("#video").attr('src', $videoSrc);
     })
-    // document ready  
 });
